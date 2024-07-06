b91e1f13a3cc17985b9acc7347a7f173321f8f42
Utkarsh-Web-2023 <utkarshweb2023@google.com>
2024-07-06 22:13:13 +0530

    app released

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-function makeGetRequest(url) {
-	fetch(url)
-		.then((response) => {
-			console.log("Fetching Github Account");
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			return response.text();
-		})
-		.then((data) => {
-			console.log("Fetched Github Account");
-		})
-		.catch((error) => {
-			console.error(
-				"There was a problem with the fetch operation:",
-				error,
-			);
-		});
+const axios = require("axios");
+
+async function makeGetRequest(url) {
+	try {
+		console.log("Fetching Github Account");
+		const response = await axios.get(url);
+		console.log("Fetched Github Account");
+	} catch (error) {
+		console.error("Error making GET request:", error.message);
+		if (error.response) {
+			console.error("Response Status:", error.response.status);
+			console.error("Response Data:", error.response.data);
+		}
+	}
 }
 
 // Example usage
 const websiteUrl = "https://github.com/Developer-Utkarsh/";
-const intervalInMilliseconds = 45000; // 30 seconds
+const intervalInMilliseconds = 30000; // 30 seconds
 
 // Set up the interval
 setInterval(() => {
